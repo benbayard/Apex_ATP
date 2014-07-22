5de52498f69039ae89ad7607d44faf6d1bcdcb3f
Velammal <Velammal.A@cognizant.com>
2014-07-22 19:15:54 +0530

    Added carousel component to content page

--- scripts/pages/content.ts
+++ scripts/pages/content.ts
@@ -2,5 +2,8 @@ $("./body"){
 	add_class("mw-details")
 	$('./div[@id="container"]/div[@class="page"]'){
 		add_class_to("./h2","mw-h1")
+		add_class_to(".//p", "mw-content-style")
+		add_class_to(".//div", "mw-content-style")
+		add_class_to(".//ol", "mw-content-style")
 	}
 }
