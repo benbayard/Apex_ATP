1ad64ade98ee2ae3e6b7d8d40a297820ba36db97
Palanisamy Thangamuthu <palanisamy.t@cognizant.com>
2014-07-30 16:27:25 +0530

    updated main and sub carousel arrows

--- scripts/pages/photos.ts
+++ scripts/pages/photos.ts
@@ -4,15 +4,19 @@ $("./body"){
 	    $("./div[@class='left rounded-8']"){
 	    	$("./div[@class='buttons']"){
 				add_class_to("./div[@id='floating-box']","mw-hide")
-				add_class_to("./a[@class='prev']","sprites-icon-L-left_arrow")
-				add_class_to("./a[@class='next']","sprites-icon-L-right_arrow")
+				add_class_to("./a[@class='prev']","sprites-icon-S-left_arrow")
+				add_class_to("./a[@class='next']","sprites-icon-S-right_arrow")
 			}
 			$("./div[@class='pic rounded-8']"){
 				add_class("mw-photo-frame")
-				add_class_to("./img","mw-photo-main")
-				insert_top("div",class:"mw-buttons"){
-					move_here("./../../div[@class='buttons']/a","top")
-				}
+				add_class_to("./div[@class='thumbnail']/img","mw-photo-main")
+                move_here("../div[@class='buttons']/a","top"){
+                    match(index(),1){
+                        wrap("div", class: 'prev_main')
+                    }match(index(),2){
+                        wrap("div", class: 'next_main')
+                    }
+                }
 			}
 			add_class_to("./h1","mw-h1")
 		}
@@ -21,7 +25,8 @@ $("./body"){
 			add_class("mw-browse-share")
 			add_class_to("./div[@class='pics rounded-8']","mw-browse")
 			add_class_to("./div[@class='f-page']","mw-share"){
-				hide('./div[@class="textwidget"][2]')
+                add_class_to('./h2', 'mw-share-title')
+                hide('./div[@class="textwidget"][2]')
 			}
 		}
 		$('./div[not(@class)]/div[@class="textwidget"][1]'){
