20baa3e03c3ae85880dbb44566cebcacac9cb277
Palanisamy Thangamuthu <palanisamy.t@cognizant.com>
2014-08-08 11:02:25 +0530

    updated width of the top advertisement

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -2,7 +2,7 @@ $('./body') {
 	 insert_top("header", class: "mw-header") {
 	 	remove("../div[@id='header']/div[@class='wrapper']/div[@class='ad']")
 	 	insert('div', class:  "mw-google-ad"){
-	 		insert_anchor_ad(320, 100, "8577018380")
+	 		insert_anchor_ad(300, 100, "8577018380")
 	 	}
 	}
 }
